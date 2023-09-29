3a9dc48bd9a0d7416a872d347343fd565238f39d
Lazerneek123 <Lazerneek123@yandex.ru>
2023-09-30 00:06:15 +0300

    The completed code

--- src/main.adb
+++ src/main.adb
@@ -2,33 +2,39 @@ with Ada.Text_IO;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Containers.Vectors;
 
-
 procedure Main is
-   type IntVector is array (Positive range <>) of Integer;
    Count_Wave : Integer := 1;
+   Count_Number : Integer :=11;
+   Size : Float := Float(Count_Number);
+   Coun : Integer := 0;
+
+   procedure Calculate (Data: Integer);
+
+   procedure Calculate (Data: Integer) is
 
    package Data_Vectors is new Ada.Containers.Vectors (Index_Type => Natural, Element_Type => Integer);
    use Data_Vectors;
    Vector_Data : Data_Vectors.Vector;
 
-   task type Task_Wave is
-      entry Start;
-   end Task_Wave;
+   task type Task_Wave;
+   type Task_Ptr is access all Task_Wave;
+   type Task_Array is array (1..Data) of Task_Ptr;
+
+   Tasks : Task_Array;
 
    task body Task_Wave is
-   function Sum (Num1, Num2 : Integer) return Integer is
+      function Sum (Num1, Num2 : Integer) return Integer is
    begin
-         Ada.Text_IO.Put_Line (Integer'Image (Num1) & " + " & Integer'Image (Num2));
+         Put_Line (Integer'Image (Num1) & " + " & Integer'Image (Num2));
          return Num1 + Num2;
       end Sum;
       Result : Data_Vectors.Vector;
       sum1 : Integer;
 
-   begin
-
-      accept Start do
-         Ada.Text_IO.Put_Line ("Wave: " & Integer'Image (Count_Wave));
-         Count_Wave := Count_Wave + 1;
+      begin
+      Put_Line ("Pre-existing array: " & Data_Vectors.Vector'Image (Vector_Data));
+      Put_Line ("Wave: " & Integer'Image (Count_Wave));
+      Count_Wave := Count_Wave + 1;
 
          for I in 1 .. (Integer(Vector_Data.Length) + 1) / 2 loop
           if Integer(Vector_Data.Length) = 1 then
@@ -45,22 +51,26 @@ procedure Main is
          Put_Line (Data_Vectors.Vector'Image (Vector_Data));
          Result.Clear;
 
-      end Start;
+      end Task_Wave;
 
-   end Task_Wave;
+   begin
 
-   Task1 : Task_Wave;
-   Task2 : Task_Wave;
-   Task3 : Task_Wave;
+    for I in 1 .. Count_Number loop
+      Vector_Data.Append(I);
+    end loop;
 
+    for I in Tasks'Range loop
+      Tasks(I) := new Task_Wave;
+    end loop;
+
+   end Calculate;
 
 begin
-   for I in 1 .. 6 loop
-      Vector_Data.Append(I);
+   while Size >= 1.0 loop
+      Size := Size / 2.0;
+      Coun := Coun + 1;
    end loop;
 
-     Task1.Start;
-     Task2.Start;
-     Task3.Start;
+   Calculate(Coun);
 
 end Main;
